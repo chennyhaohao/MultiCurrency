c13be85708eb9e92c3693c8a685f90d7e4e4df9a
chennyhaohao <chennyhaohao@gmail.com>
2018-03-20 00:55:00 +0400

    DemoToken reservation functionality+testing

--- multi_currency/controller/TokenController.js
+++ multi_currency/controller/TokenController.js
@@ -30,8 +30,67 @@ class TokenController {
 			this.tokenAddress = instance.address;
 	        return instance.address;
     	} catch (e) {
+    		console.log(e);
     		throw new Error(e);
     	}
+	}
+
+	async issueToken(to, amount, reserved) {
+		try {
+			var value = web3.toWei(amount, "ether"); //Unit translation
+	        var tokenInstance = await this.DemoToken.at(this.tokenAddress);
+	        var result = await tokenInstance.issue(to, value, reserved, {
+	            from: this.account
+	        });
+	        console.log(result);
+	        return result.tx;
+		} catch(e) {
+			//console.log(e);
+			throw new Error(e);
+		}
+	}
+
+	async reserve(to, amount) {
+		try {
+			var value = web3.toWei(amount, "ether"); //Unit translation
+	        var tokenInstance = await this.DemoToken.at(this.tokenAddress);
+	        var result = await tokenInstance.reserve(to, value, {
+	            from: this.account
+	        });
+	        console.log(result);
+	        return result.tx;
+		} catch(e) {
+			//console.log(e);
+			throw new Error(e);
+		}
+	}
+
+	async cancelRsvp(to, amount) {
+		try {
+			var value = web3.toWei(amount, "ether"); //Unit translation
+	        var tokenInstance = await this.DemoToken.at(this.tokenAddress);
+	        var result = await tokenInstance.cancelRsvp(to, value, {
+	            from: this.account
+	        });
+	        console.log(result);
+	        return result.tx;
+		} catch(e) {
+			//console.log(e);
+			throw new Error(e);
+		}
+	}	
+
+	async balanceOf(addr) {
+		try {
+			var tokenInstance = await this.DemoToken.at(this.tokenAddress);
+	        var balance = await tokenInstance.balanceOf.call(addr, 
+	            {from: this.account});
+	        console.log("balance: ", web3.fromWei(balance.valueOf(), "ether"));
+	        return web3.fromWei(balance.valueOf(), "ether");
+		} catch(e) {
+			console.log(e);
+			throw new Error(e);
+		}
 	}	
 }
 /*
@@ -46,7 +105,74 @@ web3.eth.getAccounts( (err, accs) => {
 		controller = new TokenController(accs[0]);
 	}
 });*/
+
+
 var controller = new TokenController('0x246682b5d38a4e791be9b75d7fec8a4e9680307d');
-controller.deployToken();
+/*
+controller.deployToken()
+	.then(res => controller.balanceOf('0xc48c902b59c5aea72664c9d60b30fde6fae03a44'))
+	.then(res => controller.reserve('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		100))
+	.then(res => controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		100, true))
+	.then(res => controller.balanceOf('0xc48c902b59c5aea72664c9d60b30fde6fae03a44'));
+*/
+async function test() {
+	await controller.deployToken();
+	controller.balanceOf('0xc48c902b59c5aea72664c9d60b30fde6fae03a44');
+	try { //Cannot issue more than reserved
+		await controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		100, true);
+		console.log("Reserved issue error");
+	} catch(e) {
+		console.log("Expected error: Cannot issue more than reserved");
+	}
+	try { //Cannot reserve more than cap
+		await controller.reserve('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		1001);
+		console.log("Reservation error");
+	} catch(e) {
+		console.log("Expected error: Cannot reserve more than cap");
+	}
+	await controller.reserve('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		100);
+	try { //Cannot reserve more than cap
+		await controller.reserve('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		901);
+		console.log("Reservation error");
+	} catch(e) {
+		console.log("Expected error: Cannot reserve more than cap");
+	}
+	await controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		50, true);
+	controller.balanceOf('0xc48c902b59c5aea72664c9d60b30fde6fae03a44');
+	try { //Cannot cancel more than available reservation
+		await controller.cancelRsvp('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		51, true);
+		console.log("cancelRsvp error");
+	} catch(e) {
+		console.log("Expected error: Cannot cancel more than available reservation");
+	}
+	await controller.cancelRsvp('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		50, true);
+	try { //Cannot issue more than reserved
+		await controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		1, true);
+		console.log("Reserved issue error");
+	} catch(e) {
+		console.log("Expected error: Cannot issue more than reserved");
+	}
+	await controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		1, false);
+	controller.balanceOf('0xc48c902b59c5aea72664c9d60b30fde6fae03a44');
+	try { //Cannot contribute more than cap
+		await controller.issueToken('0xc48c902b59c5aea72664c9d60b30fde6fae03a44',
+		951, false);
+	} catch(e) {
+		console.log("Expected error: Cannot contribute more than cap");
+	}
+}
+
+test();
 
 module.exports = controller;
--- multi_currency/controller/contracts/DemoToken.sol
+++ multi_currency/controller/contracts/DemoToken.sol
@@ -3,14 +3,41 @@ import './Ownable.sol';
 
 contract DemoToken is StandardToken, Ownable{
 	uint8 public decimals = 18;
+	uint256 maxCap = 1000 * (10**uint256(decimals));
+	uint256 tokenSold = 0;
+	mapping(address => uint256) tokenReserved;
 	event Issue(address recipient, uint256 amount);
+
 
 	function DemoToken() public {
 		owner = msg.sender;
 		balances[msg.sender] = (10**uint256(decimals)).mul(100);
 	}
 
-	function issue(address recipient, uint256 amount) public onlyOwner {
+	function reserve(address recipient, uint256 amount) public onlyOwner {
+		require(amount > 0);
+		require(tokenSold.add(amount) <= maxCap); //Cannot exceed cap
+		tokenSold = tokenSold.add(amount);
+		tokenReserved[recipient] = tokenReserved[recipient].add(amount);
+	}
+
+	function cancelRsvp(address recipient, uint256 amount) public onlyOwner {
+		require(amount > 0);
+		require(amount <= tokenReserved[recipient]); //When there's not enough to cancel
+		tokenReserved[recipient] = tokenReserved[recipient].sub(amount);
+		tokenSold = tokenSold.sub(amount);
+	}
+
+	function issue(address recipient, uint256 amount, bool reserved) 
+	public onlyOwner {
+		if (reserved) {
+			require(amount <= tokenReserved[recipient]); //Cannot contribute more than reserved
+			tokenReserved[recipient] = tokenReserved[recipient].sub(amount);
+			//This part of reservation is spent
+		} else {
+			require(tokenSold.add(amount) <= maxCap); //Cannot exceed cap
+			tokenSold = tokenSold.add(amount);
+		}
 		balances[recipient] = balances[recipient].add(amount);
 		totalSupply = totalSupply.add(amount);
 		Issue(recipient, amount);
