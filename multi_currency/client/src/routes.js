d6be2fb0753028b70378c181903ce52f6e11f8ba
LAPTOP-LVF36ML7\Krzysztof Reniecki <polkom21@gmail.com>
2018-03-30 16:02:52 +0200

    Implement protecting part of app

--- multi_currency/client/src/routes.js
+++ multi_currency/client/src/routes.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './components/Home/HomeComponent';
 import Login from './components/Login/LoginComponent';
 
+const Auth = {
+    isAuthenticated: false,
+    username: null,
+    token: null,
+
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            Auth.isAuthenticated ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+);
+
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Home} />
+            <PrivateRoute exact path="/" component={Home} />
             <Route path="/login" component={Login} />
         </Switch>
     </BrowserRouter>
 );
 
-export default Routes;
+export { Routes, Auth };
