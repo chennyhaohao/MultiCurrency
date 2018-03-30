63e8bc4656133fae2e18bf560003889bfa66e1ea
LAPTOP-LVF36ML7\Krzysztof Reniecki <polkom21@gmail.com>
2018-03-30 16:17:25 +0200

    Create login component

--- multi_currency/client/src/components/Login/LoginComponent.js
+++ multi_currency/client/src/components/Login/LoginComponent.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Auth } from '../../routes';
+import '../../App.css';
 
 class LoginComponent extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: '',
+            password: '',
+            redirectLogin: false,
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    login() {
+        console.log(this.state);
+        Auth.authenticate(this.state);
+        this.setState({ redirectLogin: Auth.isAuthenticated });
+    }
+
     render() {
+        if (this.state.redirectLogin) {
+            return <Redirect to="/" />;
+        }
         return (
-            <div><h1>Login Component</h1></div>  
+            <div className="loginForm">
+                <h1>Login</h1>
+                <label>Username</label>
+                <input type="text" name="login" placeholder="Username" onChange={this.onChange} />
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Password" onChange={this.onChange} />
+                <input type="submit" value="Login" onClick={this.login} />
+                <Link to="/register">Register</Link>
+            </div>  
         );
     }
 }
