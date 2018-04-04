7f9ebd6d58e08e09fb8ee5dbac42ba2b47c2a1cb
unknown <chennyhaohao@gmail.com>
2018-04-04 18:27:17 +0400

    clean up

--- multi_currency/routes/users.js
+++ multi_currency/routes/users.js
@@ -26,51 +26,6 @@ const dbParams = {
 
 var con = mysql.createConnection(dbParams);
 
-function decodeToken(token, callback) {
-    const payload = jwt.decode(token, process.env.JWT_SECRET);
-    const now = moment().unix();
-
-    if (payload == null) callback("Invalid token");
-    else if (now > payload.exp) callback("Token has expired");
-    else callback(null, payload);
-}
-
-function encodeToken(user) {
-    const payload = {
-        exp: moment().add(14, 'days').unix(),
-        iat: moment().unix(),
-        sub: user.id
-    }
-    return jwt.sign(payload, process.env.JWT_SECRET);
-}
-
-function ensureAuthorized(req, res, next) {
-    var bearerToken;
-    var bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== 'undefined') {
-        var bearer = bearerHeader.split(" ");
-        bearerToken = bearer[1];
-        decodeToken(bearerToken, (err, payload) => {
-            if (err) {
-                return res.status(401).json({ status: "Token has expired" });
-            } else {
-                con.query("SELECT * FROM users WHERE id=?", [payload.sub], (err, user) => {
-                    if (err)
-                        res.status(500).json({ status: err });
-                    else if (user.length === 0)
-                        res.status(500).json({ status: 'Invalid token' });
-                    else {
-                        req.user = user;
-                        next();
-                    }
-                });
-            }
-        });
-    } else {
-        res.status(403).json({ status: "Please log in" });
-    }
-}
-
 router.get('/', Auth.ensureAuthorized, function (req, res, next) {
 	//var result = controller.increment();
     console.log(req.user);
